4b907d9e803c4bd45c22b4636aa70c03ae03c048
Victor Ngeno <ngenovictor321@gmail.com>
2020-02-27 09:38:20 +0300

    add data

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var countyMap = L.map('county-map-window').setView(
+    [0, 0], 10
+);
+var apiToken = "pk.eyJ1Ijoibmdlbm92aWN0b3IzMjEiLCJhIjoiY2lmeWhzejVlMDJoaXU0bHp2NDcza2NteiJ9.xbQkW7UhxjnDXZT9ZQS1Rg"
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + apiToken, {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    accessToken: apiToken
+}).addTo(countyMap);
